8050c2b6284dd55885048bd513b02c7d785f8fb9
moazalhomsi <moaz.alhomsi@outlook.com>
2022-05-16 21:21:21 +0200

    Updates

--- EDA.py
+++ EDA.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+
 import seaborn as sns
 import matplotlib.pyplot as plt 
 import matplotlib
--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 # Core pkgs
 import streamlit as st
+
+st.set_page_config(page_title='Car-Evaluation',
+			   	   page_icon='🏎️')
+			   	   
 from EDA import run_EDA_app
 from ML import run_ML_app
 
 
 
-def main() :
-	st.set_page_config(page_title='Car-Evaluation',
-				   	   page_icon='🏎️')
 
 
 
-	menu = ["Home","EDA","ML"]
-	choice = st.sidebar.selectbox("Menu",menu)
 
-	if choice == "Home":
-		st.header("Welcome to Car-Eval Model")
 
-		st.markdown("""
-			The model evaluates cars according to the following concept structure:
+menu = ["Home","EDA","ML"]
+choice = st.sidebar.selectbox("Menu",menu)
+
+if choice == "Home":
+	st.header("Welcome to Car-Eval Model")
+
+	st.markdown("""
+		The model evaluates cars according to the following concept structure:
 
 CAR car acceptability
 
@@ -42,15 +45,13 @@ CAR car acceptability
 . . safety estimated safety of the car
 
 Car Evaluation Database was derived from a simple hierarchical decision model originally developed for the demonstration of DEX, M. Bohanec, V. Rajkovic: Expert system for decision making. Sistemica 1(1), pp. 145-157, 1990.)
-			""")
-		st.markdown("https://archive.ics.uci.edu/ml/datasets/Car+Evaluation")
+		""")
+	st.markdown("https://archive.ics.uci.edu/ml/datasets/Car+Evaluation")
 
-	elif choice == "EDA":
-		run_EDA_app()
+elif choice == "EDA":
+	run_EDA_app()
 
-	elif choice == "ML":
-		run_ML_app() 
+elif choice == "ML":
+	run_ML_app() 
     
 
-if __name__ == '__main__':
-	main()
